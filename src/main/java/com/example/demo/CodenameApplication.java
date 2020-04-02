d10dfd6d2ebbd213803c2685331501f3b50f354d
lasbleiso <olivier.lasbleis@brittany-ferries.fr>
2020-04-02 12:56:43 +0200

    first

---
+++ src/main/java/com/example/demo/CodenameApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodenameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodenameApplication.class, args);
+	}
+
+}
